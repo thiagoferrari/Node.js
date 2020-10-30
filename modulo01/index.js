85816950e4a82eeda17cd004e36807d4e9efd380
thiagoferrari <thiagofcjr@gmail.com>
2020-10-30 15:30:34 -0300

    Query Params + Route Params

--- modulo01/index.js
+++ modulo01/index.js
@@ -4,10 +4,39 @@ const express = require('express')
 // iniciando servidor express:
 const server = express()
 
-//passa pelo GET nenhum req e responde Hello World
+
+/* --------------- 'Hello World' --------------- */
+
+/* // por meio de um GET na rota '/teste', res responde 'Hello World'
 server.get('/teste', (req, res) => {
     return res.json({ message: 'Hello World' })
-})
+}) */
 
-// inicia o servidor ouvindo a porta 3000
+
+/* --------------- Parâmetros --------------- */
+
+// LEMBRE-SE: NO TOTAL TEM 3 FORMAS DE PASSAR PARÂMETRO NA WEB:
+// Query params = ...rota?nome=1
+// Route params = /users/1
+// Request body = { "name":"thiago", "email":"thiagofcjr@gmail.com"} - JSON
+
+/* -------- Exemplos --------- */
+
+// Ex. de Query params:
+server.get('/teste', (req, res) => {
+    let { nome } = req.query //desestruturado
+    return res.json({ message: `Hello ${nome} tudo 100% na rota?` })
+})
+// coloque no browser: localhost:3000/teste?nome=thiago
+
+/* ------------ */
+
+// Ex. de Route params:
+server.get('/users/:id', (req, res) => {
+    const { id } = req.params //desestruturado
+    return res.json({ message: `Buscando usuário ${id}` })
+})
+// coloque no browser: localhost:3000/users/7
+
+// inicia o servidor ouvindo=(permitindo requisições) na porta 3000
 server.listen(3000)
