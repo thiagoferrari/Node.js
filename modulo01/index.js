c9d1b91b2252a53ba9fbc94b14d2f69b1b2751c2
Thiago Ferrari <thiagofcjr@gmail.com>
2020-12-07 22:12:18 -0300

    CRUD com rotas

--- modulo01/index.js
+++ modulo01/index.js
@@ -1,8 +1,12 @@
 /*packge.json armazena todas as dependencias do projeto*/
 
+const { json } = require("express");
 const express = require("express");
 
 const server = express()
+
+// abaixo uso um plugin para ler json em req. POST
+server.use(express.json())
 
 // PASSAGEM DE PARÂMETROS NA WEB:
 // Query params = ?teste=1
@@ -10,8 +14,8 @@ const server = express()
 // Request body = { "name": "Thiago", "email": "thiago@gmail.com" }
 
 
-// CONSUMINDO DE QUERY PARAMS: (digite http://localhost:3000/teste?nome=Thiago)
-
+// ROTA PARA FALAR HELLO (digite http://localhost:3000/teste?nome=Thiago)
+//*CONSUMINDO DE QUERY PARAMS:
 server.get('/teste', (req, res) => {
     //const nome = req.query.nome
     const { nome } = req.query
@@ -20,11 +24,14 @@ server.get('/teste', (req, res) => {
 })
 
 
+// CRUD ABAIXO PRONTO:
 
-// CONSUMINDO DE ROUTE PARAMS: (digite http://localhost:3000/users/3)
+// ROTA PARA LISTAR UM USUÁRIO (digite http://localhost:3000/users/3)
+//*CONSUMINDO DE ROUTE PARAMS:
 const users = ['Thiago', 'Mateus', 'Victor']
+
 server.get('/users/:index', (req, res) => {
-    
+
     // pegando o user a partir do index inserido no array:
     const { index } = req.params
 
@@ -32,4 +39,43 @@ server.get('/users/:index', (req, res) => {
 })
 
 
+// ROTA PARA LISTAR TODOS OS USUÁRIOS: (digite http://localhost:3000/users)
+
+server.get('/users', (req, res) => {
+    return res.json(users)
+})
+
+
+// ROTA PARA CRIAR USUÁRIOS: (digite http://localhost:3000/users) - JSON contem name
+server.post('/users', (req, res) => {
+    const { name } = req.body
+
+    users.push(name)
+
+    return res.json(users)
+})
+
+
+// ROTA PARA ALTERAR USUÁRIOS: 
+server.put('/users/:index', (req, res) => {
+    const { index } = req.params
+
+    const { name } = req.body
+
+    users[index] = name
+
+    return res.json(users)
+})
+
+// ROTA PARA DELETAR USUÁRIOS: 
+server.delete('/users/:index', (req, res) => {
+    const { index } = req.params
+
+    users.splice(index, 1)
+
+    return res.send()
+})
+
+
+// PORTA QUE ESPERA URL COM ROTAS PREENCHIDAS
 server.listen(3000)
